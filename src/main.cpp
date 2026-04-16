a238beb6eff765ea199784c5a733eeca7bee8348
lyyako <lyyako@proton.me>
2026-04-16 20:42:36 +0300

    16

--- src/main.cpp
+++ src/main.cpp
@@ -146,6 +146,29 @@ static void tryUpdateMuteButton(CCLayer* pLayer, bool pIsMusic) {
     toggler->toggle(false);
 }
 
+static float getCorrectedVolume(float incomingVolume, const char* gameVariableKey) {
+    constexpr float BUGGY_VOLUME_THRESHOLD = 0.03f;
+    if (incomingVolume == 0.f) {
+        float realVolume = GameManager::sharedState()->getGameVariable(gameVariableKey);
+        if (realVolume > 0.f && realVolume < BUGGY_VOLUME_THRESHOLD) {
+            return realVolume;
+        }
+    }
+    return incomingVolume;
+}
+
+class $modify(FMODAudioEngine) {
+    void setBackgroundMusicVolume(float volume) {
+        float correctedVolume = getCorrectedVolume(volume, "0032");
+        FMODAudioEngine::setBackgroundMusicVolume(correctedVolume);
+    }
+
+    void setEffectsVolume(float volume) {
+        float correctedVolume = getCorrectedVolume(volume, "0034");
+        FMODAudioEngine::setEffectsVolume(correctedVolume);
+    }
+};
+
 class $modify(PauseLayer) {
     struct Fields { 
         TextInput* m_musicInput = nullptr; 
@@ -162,25 +185,19 @@ class $modify(PauseLayer) {
     }
 
     void musicSliderChanged(CCObject* pSender) {
+        PauseLayer::musicSliderChanged(pSender);
         auto fields = m_fields.self();
         if (fields->m_musicInput && fields->m_musicSlider) {
-            float sliderValue = fields->m_musicSlider->getValue();
-            GameManager::sharedState()->setGameVariable("0032", sliderValue);
-            FMODAudioEngine::sharedEngine()->setBackgroundMusicVolume(sliderValue);
-
-            fields->m_musicInput->setString(getVolumeStr(sliderValue));
+            fields->m_musicInput->setString(getVolumeStr(fields->m_musicSlider->getValue()));
             tryUpdateMuteButton(this, true);
         }
     }
 
     void sfxSliderChanged(CCObject* pSender) {
+        PauseLayer::sfxSliderChanged(pSender);
         auto fields = m_fields.self();
         if (fields->m_sfxInput && fields->m_sfxSlider) {
-            float sliderValue = fields->m_sfxSlider->getValue();
-            GameManager::sharedState()->setGameVariable("0034", sliderValue);
-            FMODAudioEngine::sharedEngine()->setEffectsVolume(sliderValue);
-
-            fields->m_sfxInput->setString(getVolumeStr(sliderValue));
+            fields->m_sfxInput->setString(getVolumeStr(fields->m_sfxSlider->getValue()));
             tryUpdateMuteButton(this, false);
         }
     }
@@ -202,25 +219,19 @@ class $modify(OptionsLayer) {
     }
 
     void musicSliderChanged(CCObject* pSender) {
+        OptionsLayer::musicSliderChanged(pSender);
         auto fields = m_fields.self();
         if (fields->m_musicInput && fields->m_musicSlider) {
-            float sliderValue = fields->m_musicSlider->getValue();
-            GameManager::sharedState()->setGameVariable("0032", sliderValue);
-            FMODAudioEngine::sharedEngine()->setBackgroundMusicVolume(sliderValue);
-
-            fields->m_musicInput->setString(getVolumeStr(sliderValue));
+            fields->m_musicInput->setString(getVolumeStr(fields->m_musicSlider->getValue()));
             tryUpdateMuteButton(this, true);
         }
     }
 
     void sfxSliderChanged(CCObject* pSender) {
+        OptionsLayer::sfxSliderChanged(pSender);
         auto fields = m_fields.self();
         if (fields->m_sfxInput && fields->m_sfxSlider) {
-            float sliderValue = fields->m_sfxSlider->getValue();
-            GameManager::sharedState()->setGameVariable("0034", sliderValue);
-            FMODAudioEngine::sharedEngine()->setEffectsVolume(sliderValue);
-
-            fields->m_sfxInput->setString(getVolumeStr(sliderValue));
+            fields->m_sfxInput->setString(getVolumeStr(fields->m_sfxSlider->getValue()));
             tryUpdateMuteButton(this, false);
         }
     }
